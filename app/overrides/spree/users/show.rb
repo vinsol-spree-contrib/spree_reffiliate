542ea479b61780edb5638442c7825855109f7af6
Sawan Gupta <sawan.gupta92@gmail.com>
2016-11-16 16:24:38 +0530

    Update UI for my accounts page

--- app/overrides/spree/users/show.rb
+++ app/overrides/spree/users/show.rb
@@ -4,9 +4,22 @@ Deface::Override.new(
   insert_after: "[data-hook='account_summary'] #user-info dd:first"
 ) do
 <<-CODE.chomp
+<br>
 <dt>Referral URL</dt>
-<dd><input type='text' value='<%= referral_url(@user.referral.code) %>' onClick='this.select();' class='form-control' /></dd>
-<dt>Referred Users</dt>
-<dd><%= @user.referred_count%></dd>
+<dd class='col-md-6 row'>
+  <div class="input-group">
+    <input type='text' data-coupon-code=true value='<%= referral_url(@user.referral.code) %>' class='form-control' />
+    <span class="input-group-btn">
+      <button class="btn btn-primary" data-copy-coupon-code=true type="button" title='Copy to clipboard'><span class='glyphicon glyphicon-copy'></span></button>
+    </span>
+  </div>
+</dd>
+<br>
+<br>
+<br>
+<div>
+  <dt>Referred Users</dt>
+  <dd><%= @user.referred_count%></dd>
+</div>
 CODE
 end
