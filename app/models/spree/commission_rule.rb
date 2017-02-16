6a3da75c7184f5218ff86f24c49902af63ca2dec
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-02-16 11:00:55 +0530

    moved commission_rule names to constant

--- app/models/spree/commission_rule.rb
+++ app/models/spree/commission_rule.rb
@@ -1,17 +1,18 @@
 module Spree
   class CommissionRule < Spree::Base
-
+    USER_REGISTRATION = 'User Registration'
+    ORDER_PLACEMENT = 'Order Placement'
     has_many :affiliate_commission_rules, dependent: :restrict_with_error
     has_many :affiliate, through: :affiliate_commission_rules
 
     validates :name, presence: true
 
     def self.user_registration
-      find_by(name: 'User Registration')
+      find_by(name: USER_REGISTRATION)
     end
 
     def self.order_placement
-      find_by(name: 'Order Placement')
+      find_by(name: ORDER_PLACEMENT)
     end
   end
 end
